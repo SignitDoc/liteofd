53c76af5e0ca60980ab5e375dd626b4d79e08da3
xxss0903 <xxss0903@gmail.com>
2025-07-08 18:24:51 +0800

    fix: 增加pathobject的绘制，绘制线条

--- src/liteofd/canvasContentLayer.ts
+++ src/liteofd/canvasContentLayer.ts
@@ -6,7 +6,7 @@ import { AttributeKey, OFD_KEY } from "./attrType"
 import { fontIdWithName, opentypeFonts } from "./ofdFont"
 import { findAttributeValueByKey } from "./parser"
 import { getFontSize, getCTM, parseColor } from "./utils/elementUtils"
-import { convertToBox, convertToDpi } from "./utils/utils"
+import { convertToBox, convertToDpi, calPathPoint, convertPathAbbreviatedDatatoPoint } from "./utils/utils"
 import opentype from "../opentype"
 
 // 使用canvas进行绘制界面
@@ -41,13 +41,16 @@ export class CanvasContentLayer extends Layer {
 		}
 		// 获取canvas上下文
 		if (!this.pageCanvasCtx) {
-			this.pageCanvasCtx = this.pageCanvas.getContext('2d')
-			console.error("无法获取canvas上下文")
-			return
+			const ctx = this.pageCanvas.getContext('2d')
+			if (!ctx) {
+				console.error("无法获取canvas上下文")
+				return
+			}
+			this.pageCanvasCtx = ctx
 		}
 		// 获取文本位置
 		let boundaryStr = parser.findAttributeValueByKey(nodeData, AttributeKey.Boundary)
-		let boundaryBox = null
+		let boundaryBox: { x: number; y: number; width: number; height: number; } | null = null
 		if (boundaryStr) {
 			 boundaryBox = convertToBox(boundaryStr)
 		}
@@ -173,14 +176,287 @@ export class CanvasContentLayer extends Layer {
 		}
 	}
 
+	/**
+	 * 渲染路径path
+	 * @param nodeObjs
+	 * @param pageContainer
+	 * @private
+	 */
+	#renderPathObject(nodeObjs: XmlData, pageContainer: Element) {
+		// 多个path
+		for (let i = 0; i < nodeObjs.children.length; i++) {
+			let nodeData = nodeObjs.children[i]
+			this.#renderSingplePathObject(nodeData, pageContainer)
+		}
+	}
+
+	#renderSingplePathObject(nodeData: XmlData, pageContainer: Element) {
+		// 获取canvas上下文
+		if (!this.pageCanvasCtx) {
+			const ctx = this.pageCanvas.getContext('2d')
+			if (!ctx) {
+				console.error("无法获取canvas上下文")
+				return
+			}
+			this.pageCanvasCtx = ctx
+		}
+
+		let boundaryStr = parser.findAttributeValueByKey(nodeData, AttributeKey.Boundary)
+		let boundaryBox: { x: number; y: number; width: number; height: number; } | null = null
+		if (boundaryStr) {
+			boundaryBox = convertToBox(boundaryStr)
+		}
+
+		// 获取路径数据
+		let abbreviatedData = parser.findValueByTagNameOfFirstNode(nodeData, OFD_KEY.AbbreviatedData)
+		if (!abbreviatedData) {
+			return
+		}
+		// 计算路径点
+		const points = calPathPoint(convertPathAbbreviatedDatatoPoint(abbreviatedData.value))
+		// 设置路径样式
+		this.#setCanvasPathStyle(nodeData)
+		// 应用CTM变换
+		this.#applyCTMTransform(this.pageCanvasCtx, nodeData)
+		// 绘制路径 - 在boundaryBox位置绘制
+		this.#drawCanvasPath(points, boundaryBox)
+		console.log("Canvas绘制路径:", nodeData, "点数:", points.length, "boundaryBox:", boundaryBox)
+	}
+
+	// 设置canvas路径样式
+	#setCanvasPathStyle(nodeData: XmlData) {
+		const ctx = this.pageCanvasCtx
+
+		// 设置线宽
+		let lineWidthStr = parser.findAttributeValueByKey(nodeData, AttributeKey.LineWidth)
+		if (lineWidthStr) {
+			let lineWidth = convertToDpi(parseFloat(lineWidthStr))
+			ctx.lineWidth = lineWidth
+		} else {
+			ctx.lineWidth = 1
+		}
+
+		// 设置虚线模式
+		const dashPattern = parser.findAttributeValueByKey(nodeData, AttributeKey.DashPattern)
+		if (dashPattern) {
+			const dashArray = dashPattern.split(' ').map(value => convertToDpi(parseFloat(value)))
+			ctx.setLineDash(dashArray)
+		} else {
+			ctx.setLineDash([])
+		}
+
+		// 设置描边颜色
+		let strokeColorObj = parser.findValueByTagName(nodeData, OFD_KEY.StrokeColor)
+		let strokeColorBoolean = parser.findAttributeValueByKey(nodeData, AttributeKey.Stroke)
+		let strokeColorStr = strokeColorObj && parser.findAttributeValueByKey(strokeColorObj, AttributeKey.Value)
+
+		if (strokeColorBoolean && JSON.parse(strokeColorBoolean)) {
+			if (strokeColorStr) {
+				ctx.strokeStyle = parseColor(strokeColorStr)
+			}
+		} else if (strokeColorStr) {
+			ctx.strokeStyle = parseColor(strokeColorStr)
+		} else {
+			ctx.strokeStyle = 'rgb(0, 0, 0)' // 默认黑色
+		}
+
+		// 设置填充颜色
+		let fillColorObj = parser.findValueByTagName(nodeData, OFD_KEY.FillColor)
+		let fillColorBoolean = parser.findAttributeValueByKey(nodeData, AttributeKey.Fill)
+		let fillColorStr = fillColorObj && parser.findAttributeValueByKey(fillColorObj, AttributeKey.Value)
+
+		if (fillColorBoolean) {
+			if (fillColorObj && fillColorStr) {
+				ctx.fillStyle = parseColor(fillColorStr)
+			} else {
+				ctx.fillStyle = 'none'
+			}
+		} else {
+			if (fillColorStr) {
+				ctx.fillStyle = parseColor(fillColorStr)
+			} else {
+				ctx.fillStyle = 'none'
+			}
+		}
+	}
+
+		// 绘制canvas路径
+	#drawCanvasPath(points: any[], boundaryBox: { x: number; y: number; width: number; height: number; } | null = null) {
+		const ctx = this.pageCanvasCtx
+		ctx.beginPath()
+
+		// 如果有boundaryBox，设置起始位置
+		let currentX = boundaryBox ? boundaryBox.x : 0
+		let currentY = boundaryBox ? boundaryBox.y : 0
+		let firstPoint = true
+
+		for (let i = 0; i < points.length; i++) {
+			const point = points[i]
+
+			switch (point.type) {
+				case 'M': // 移动到
+					// 如果有boundaryBox，将第一个M命令的位置调整为boundaryBox位置
+					if (firstPoint && boundaryBox) {
+						ctx.moveTo(boundaryBox.x + point.x, boundaryBox.y + point.y)
+						currentX = boundaryBox.x + point.x
+						currentY = boundaryBox.y + point.y
+					} else {
+						ctx.moveTo(point.x, point.y)
+						currentX = point.x
+						currentY = point.y
+					}
+					firstPoint = false
+					break
+				case 'L': // 画线到
+					if (boundaryBox) {
+						ctx.lineTo(boundaryBox.x + point.x, boundaryBox.y + point.y)
+						currentX = boundaryBox.x + point.x
+						currentY = boundaryBox.y + point.y
+					} else {
+						ctx.lineTo(point.x, point.y)
+						currentX = point.x
+						currentY = point.y
+					}
+					break
+				case 'H': // 水平线到
+					if (boundaryBox) {
+						ctx.lineTo(boundaryBox.x + point.x, currentY)
+						currentX = boundaryBox.x + point.x
+					} else {
+						ctx.lineTo(point.x, currentY)
+						currentX = point.x
+					}
+					break
+				case 'V': // 垂直线到
+					if (boundaryBox) {
+						ctx.lineTo(currentX, boundaryBox.y + point.y)
+						currentY = boundaryBox.y + point.y
+					} else {
+						ctx.lineTo(currentX, point.y)
+						currentY = point.y
+					}
+					break
+				case 'C': // 三次贝塞尔曲线
+					if (boundaryBox) {
+						ctx.bezierCurveTo(
+							boundaryBox.x + point.x1, boundaryBox.y + point.y1,
+							boundaryBox.x + point.x2, boundaryBox.y + point.y2,
+							boundaryBox.x + point.x, boundaryBox.y + point.y
+						)
+						currentX = boundaryBox.x + point.x
+						currentY = boundaryBox.y + point.y
+					} else {
+						ctx.bezierCurveTo(point.x1, point.y1, point.x2, point.y2, point.x, point.y)
+						currentX = point.x
+						currentY = point.y
+					}
+					break
+				case 'S': // 平滑三次贝塞尔曲线
+					if (boundaryBox) {
+						if ('x2' in point && 'y2' in point) {
+							ctx.bezierCurveTo(
+								boundaryBox.x + point.x2, boundaryBox.y + point.y2,
+								boundaryBox.x + point.x2, boundaryBox.y + point.y2,
+								boundaryBox.x + point.x, boundaryBox.y + point.y
+							)
+						} else {
+							ctx.bezierCurveTo(
+								boundaryBox.x + point.x, boundaryBox.y + point.y,
+								boundaryBox.x + point.x, boundaryBox.y + point.y,
+								boundaryBox.x + point.x, boundaryBox.y + point.y
+							)
+						}
+						currentX = boundaryBox.x + point.x
+						currentY = boundaryBox.y + point.y
+					} else {
+						if ('x2' in point && 'y2' in point) {
+							ctx.bezierCurveTo(point.x2, point.y2, point.x2, point.y2, point.x, point.y)
+						} else {
+							ctx.bezierCurveTo(point.x, point.y, point.x, point.y, point.x, point.y)
+						}
+						currentX = point.x
+						currentY = point.y
+					}
+					break
+				case 'Q': // 二次贝塞尔曲线
+					if (boundaryBox) {
+						ctx.quadraticCurveTo(
+							boundaryBox.x + point.x1, boundaryBox.y + point.y1,
+							boundaryBox.x + point.x, boundaryBox.y + point.y
+						)
+						currentX = boundaryBox.x + point.x
+						currentY = boundaryBox.y + point.y
+					} else {
+						ctx.quadraticCurveTo(point.x1, point.y1, point.x, point.y)
+						currentX = point.x
+						currentY = point.y
+					}
+					break
+				case 'T': // 平滑二次贝塞尔曲线
+					if (boundaryBox) {
+						ctx.quadraticCurveTo(
+							boundaryBox.x + point.x, boundaryBox.y + point.y,
+							boundaryBox.x + point.x, boundaryBox.y + point.y
+						)
+						currentX = boundaryBox.x + point.x
+						currentY = boundaryBox.y + point.y
+					} else {
+						ctx.quadraticCurveTo(point.x, point.y, point.x, point.y)
+						currentX = point.x
+						currentY = point.y
+					}
+					break
+				case 'A': // 椭圆弧
+					// 简化处理：将椭圆弧转换为直线
+					if (boundaryBox) {
+						ctx.lineTo(boundaryBox.x + point.x, boundaryBox.y + point.y)
+						currentX = boundaryBox.x + point.x
+						currentY = boundaryBox.y + point.y
+					} else {
+						ctx.lineTo(point.x, point.y)
+						currentX = point.x
+						currentY = point.y
+					}
+					break
+				case 'Z': // 闭合路径
+				case 'z':
+					if (!firstPoint) {
+						ctx.closePath()
+					}
+					break
+				case 'B': // 自定义贝塞尔曲线
+					if (boundaryBox) {
+						ctx.bezierCurveTo(
+							boundaryBox.x + point.x1, boundaryBox.y + point.y1,
+							boundaryBox.x + point.x2, boundaryBox.y + point.y2,
+							boundaryBox.x + point.x3, boundaryBox.y + point.y3
+						)
+						currentX = boundaryBox.x + point.x3
+						currentY = boundaryBox.y + point.y3
+					} else {
+						ctx.bezierCurveTo(point.x1, point.y1, point.x2, point.y2, point.x3, point.y3)
+						currentX = point.x3
+						currentY = point.y3
+					}
+					break
+			}
+		}
+
+		// 填充和描边
+		if (ctx.fillStyle !== 'none') {
+			ctx.fill()
+		}
+		ctx.stroke()
+	}
+
 	#renderLayerDataObject(dataObj: XmlData, pageContainer: Element) {
 		switch (dataObj.tagName) {
 			case OFD_KEY.TextObject:
 				this.#renderTextObject(dataObj, pageContainer)
 				break
-			// case OFD_KEY.PathObject:
-			// 	this.#renderPathObject(dataObj, pageContainer)
-			// 	break
+			case OFD_KEY.PathObject:
+				this.#renderPathObject(dataObj, pageContainer)
+				break
 			// case OFD_KEY.ImageObject:
 			// 	this.#renderImageObject(dataObj, pageContainer)
 			// 	break
@@ -219,7 +495,12 @@ export class CanvasContentLayer extends Layer {
 			let contentData = parser.findValueByTagName(pageData, OFD_KEY.Content)
 			if(contentData) {
 				this.pageCanvas = pageContainer.firstChild as HTMLCanvasElement
-				this.pageCanvasCtx = this.pageCanvas.getContext('2d')
+				const ctx = this.pageCanvas.getContext('2d')
+				if (!ctx) {
+					console.error("无法获取canvas上下文")
+					return null
+				}
+				this.pageCanvasCtx = ctx
 				// 渲染内容层
 				this.#initPageContainer()
 				this.#renderPageContent(contentData, pageContainer)
