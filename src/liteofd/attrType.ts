249898ae97f77e7108dc0bbad8ec38cd146fe040
xxss0903 <xxss0903@gmail.com>
2025-11-07 17:11:38 +0800

    fix: ofd的属性增加值

--- src/liteofd/attrType.ts
+++ src/liteofd/attrType.ts
@@ -36,11 +36,12 @@ export const AttributeKey = {
 	Clip: "Clip", // 裁剪
 	Title: "Title", // 大纲的标题
 	PageNum: "PageNum", // 大纲的页面位置
+	Top: "Top", // actions的top位置
 	Event: "Event", // 点击事件，比如大纲中的action的事件Click
-	PageID: "PageID", // 
-	Type: "Type", // 
+	PageID: "PageID", //
+	Type: "Type", //
 	Subtype: "Subtype", // 注释的子类型
-	DashPattern: "DashPattern", // 
+	DashPattern: "DashPattern", //
 	BitsPerComponent: "BitsPerComponent", // 颜色空间每个分量的位数
 	Rule: "Rule", // 路径填充规则
 }
@@ -160,12 +161,12 @@ export const ANNOT_TYPE = {
 		value: "Path",
 		subType: {
 			Watercolor: "Watercolor", // 水印，需要在文本层上面
-			Fluorescent: "Fluorescent", // 荧光笔，在文本层上面 
+			Fluorescent: "Fluorescent", // 荧光笔，在文本层上面
 			Pen: "Pen", // 笔触，在文本层上面
 			Strikeout: "Strikeout", // 删除线，要在文本层的index上面
 			Squiggly: "Squiggly", // 下划线的弯弯曲曲类型，放在文本底部的，要在文本下面把
 		}
-	}, 
+	},
 	Stamp: {
 		value: "Stamp",
 		subType: {
@@ -173,5 +174,5 @@ export const ANNOT_TYPE = {
 			SignatureInFile: "SignatureInFile", // 印章
 		}
 	}
-	
+
 }
